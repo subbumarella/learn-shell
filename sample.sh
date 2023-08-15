ea167d02cb71b961fdb0ecfe7c026ee4f97edbb5
subbumarella <subbumarella1996@gmail.com>
2023-08-15 11:54:31 +0530

    update color fonts

--- sample.sh
+++ sample.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo  -e "\e[31mHello world in RED Colour\e[0m"
+echo -e "\e[31m Hello world in RED Colour \e[0m"
+echo -e "\e[32m Hello world in Green colour \e[0m"
+echo -e "\e[33m Hello world in Yellow colour \e[0m"
+echo -e "\e[34m Hello world in Blue colour \e[0m"
+echo -e "\e[35m Hello world in Magenta colour \e[0m"
+echo -e "\e[36m Hello world in Cyan colour \e[0m"
