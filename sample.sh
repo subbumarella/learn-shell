c4f3c6810564d764ba59d52ba17839ef2afd8475
subbumarella <subbumarella1996@gmail.com>
2023-08-15 12:18:24 +0530

    Added  frontend micro service

--- sample.sh
+++ sample.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-echo -e "\e[31m Hello world in RED Colour \e[0m"
-echo -e "\e[32m Hello world in Green colour \e[0m"
-echo -e "\e[33m Hello world in Yellow colour \e[0m"
-echo -e "\e[34m Hello world in Blue colour \e[0m"
-echo -e "\e[35m Hello world in Magenta colour \e[0m"
-echo -e "\e[36m Hello world in Cyan colour \e[0m"
+echo -e "\e[31m Installing nginx \e[0m"
+yum install nginx -y 
+
+echo -e "\e[32m Removing default pages \e[0m"
+rm -rf /usr/share/nginx/html/* 
+
+echo -e "\e[32m Installing frontend zip  \e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+
+cd /usr/share/nginx/html 
+echo -e "\e[33m Unzip the file \e[0m"
+unzip /tmp/frontend.zip
+
+# vim /etc/nginx/default.d/roboshop.conf 
+echo -e "\e[34m restarting the nginx \e[0m"
+systemctl enable nginx 
+
+systemctl restart nginx 
