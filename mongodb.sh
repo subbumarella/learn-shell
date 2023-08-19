9f4ae53e453fd7a968c9d995429469cdde81f5e9
subbumarella <54053065+subbumarella@users.noreply.github.com>
2023-08-19 20:38:57 +0530

    removed redirectional

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,10 @@
 echo -e "\e[31m Installing mongodb   \e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
 
 echo -e "\e[32m Installing mongodb   \e[0m"
-yum install mongodb-org -y &>>/tmp/roboshop.log
+yum install mongodb-org -y 
 
 echo -e "\e[33m Removing 127.0.0.0   \e[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>/tmp/roboshop.log
-systemctl enable mongod &>>/tmp/roboshop.log
-systemctl restart mongod &>>/tmp/roboshop.log
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf  
+systemctl enable mongod  
+systemctl restart mongod  
