7be13747da25c2ad59bd2c449acb61f09fff205f
subbumarella <54053065+subbumarella@users.noreply.github.com>
2023-08-23 23:48:31 +0530

    updated

--- cart.sh
+++ cart.sh
@@ -1,7 +1,6 @@
 echo -e "\e[34m ***********************Instalation starting ********************\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-
 echo -e "\e[32m Installing  NodeJS  \e[0m"
 yum install nodejs -y  
 
@@ -12,7 +11,7 @@ rm -rf /app
 mkdir /app 
 
 echo -e "\e[34m Download the application code to created app directory.  \e[0m"
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip   
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip   
 
 echo -e "\e[31m Changing directory  \e[0m"
 cd /app
@@ -27,11 +26,11 @@ echo -e "\e[34m Installing npm  \e[0m"
 npm install 
 
 echo -e "\e[35m Setup SystemD User Service \e[0m"
-cp /home/centos/learn-shell/cart.conf /etc/systemd/system/cart.service
+cp /home/centos/learn-shell/cart.service /etc/systemd/system/cart.service
 
-echo -e "\e[31m reloading  \e[0m"
 systemctl daemon-reload
-systemctl enable cart 
-systemctl start cart
+systemctl enable user 
+systemctl start user
+
 
 
