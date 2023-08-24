76b4bfb5920255ab21d45ea3db77d3d87c9d5646
subbumarella <54053065+subbumarella@users.noreply.github.com>
2023-08-24 22:45:44 +0530

    updated

--- mysql.sh
+++ mysql.sh
@@ -6,7 +6,7 @@ yum install mysql-community-server -y
 
 echo -e "\e[32m Start MySQL Service \e[0m"
 systemctl enable mysqld
-systemctl restart mysqld  
+systemctl start mysqld  
 
 echo -e "\e[33m We need to change the default root password in order to start using the database \e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
