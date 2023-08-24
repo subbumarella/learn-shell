0345fcf79d6b50d1d45c32357c204b3c0b4eb3f9
subbumarella <54053065+subbumarella@users.noreply.github.com>
2023-08-24 22:49:42 +0530

    added cp

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,8 @@
 echo -e "\e[34m ***********************Instalation starting ********************\e[0m"
 yum module disable mysql -y 
 
+cp /home/centos/learn-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+
 echo -e "\e[31m Install MySQL Server \e[0m"
 yum install mysql-community-server -y
 
@@ -10,6 +12,3 @@ systemctl start mysqld
 
 echo -e "\e[33m We need to change the default root password in order to start using the database \e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
-
-echo -e "\e[34m Start MySQL Service \e[0m"
-mysql -uroot -pRoboShop@1
